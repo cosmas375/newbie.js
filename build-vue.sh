859b3c551af4d0b95d081badda22928c6c347d37
Roman Lyakhovchik <roman.lyakhovchik@gmail.com>
2022-01-16 00:37:48 +0300

    r: vue build script logs

--- build-vue.sh
+++ build-vue.sh
@@ -1,3 +1,12 @@
+echo "Starting Vue build...";
+
 mkdir -p ./dist/vue/ && cp -r ./src/entry-vue.ts ./dist/vue/index.ts && \
 mkdir -p ./dist/vue/assets && cp -r ./src/assets ./dist/vue/ && \
 mkdir -p ./dist/vue/core && cp -r ./src/core ./dist/vue/
+
+if [ $? -ne 0 ]
+then
+    echo "Vue build failed!"
+else
+    echo "Vue build completed successfully!"
+fi
