bd618bf7478931c5732c45ae97212ba8a1a476a3
Roman Lyakhovchik <roman.lyakhovchik@gmail.com>
2022-02-12 13:32:12 +0300

    fix: e2e tests default values

--- build-vue.sh
+++ build-vue.sh
@@ -1,7 +1,7 @@
 echo "Starting Vue build...";
 
 rm -rf ./dist/vue && \
-smkdir -p ./dist/vue/ && cp -r ./src/entry-vue.ts ./dist/vue/index.ts && \
+mkdir -p ./dist/vue/ && cp -r ./src/entry-vue.ts ./dist/vue/index.ts && \
 mkdir -p ./dist/vue/assets && cp -r ./src/assets ./dist/vue/ && \
 mkdir -p ./dist/vue/core && cp -r ./src/core ./dist/vue/
 
