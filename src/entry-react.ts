1ab510081bcca06c0a61116c1f00ccac9cd8cf40
Roman Lyakhovchik <roman.lyakhovchik@gmail.com>
2022-09-27 15:33:27 +0300

    r: export position enum

--- src/entry-react.ts
+++ src/entry-react.ts
@@ -1,9 +1,12 @@
 import { Newbie } from './core/class/Newbie';
 import { Globals } from './core/class/Globals';
 import { ReactComponentsFactory } from './core/class/ComponentsFactory/ReactComponentsFactory';
+import { Position } from './core/Position';
 
 export default Newbie;
 
 export function useNewbie({ ReactDOM }) {
     Globals.componentsFactory = new ReactComponentsFactory({ ReactDOM });
 }
+
+export const NewbiePosition = Position;
